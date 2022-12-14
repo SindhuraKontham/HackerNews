1811a80b0effe8e3d008c61963b5afc581334ed3
Sindhura <sindhura.kontham@gmail.com>
2022-12-14 17:29:53 +0100

    Added styling

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function NewsItem({newsItem}) {
   return (
-    <li className='NewsItem'>
+    <li className='NewsItem p-2 bg-light border'>
       <span>{newsItem.title}</span>
       <span>{newsItem.author}</span>
       <p><a href={newsItem.url}>{newsItem.url}</a></p>
